b2ed6916a91949d26084d402952672709a2b95b8
Joshua Perrymon <josh@packetfocus.com>
2015-09-09 12:48:59 -0500

    fixed wget

--- install-modules.sh
+++ install-modules.sh
@@ -1,5 +1,5 @@
-wget https://raw.githubusercontent.com/packetfocus/EmpireModules/master/Invoke-MassMimikatz.ps1 > /tmp/Invoke-MassMimikatz.ps1
-wget https://raw.githubusercontent.com/packetfocus/EmpireModules/master/Invoke-MassMimikatz.py > /tmp/Invoke-MassMimikatz.py
+wget https://raw.githubusercontent.com/packetfocus/EmpireModules/master/Invoke-MassMimikatz.ps1 -O /tmp/Invoke-MassMimikatz.ps1
+wget https://raw.githubusercontent.com/packetfocus/EmpireModules/master/Invoke-MassMimikatz.py -O /tmp/Invoke-MassMimikatz.py
 echo "Moving into Empire Directories"
 cp  /tmp/Invoke-MassMimikatz.ps1 /tools/Empire/data/module_source/credentials/
 cp   /tmp/Invoke-MassMimikatz.py /tools/Empire/lib/modules/credentials/mimikatz/
